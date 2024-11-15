30361c4a95ba2cc7a5b964047c6cb7faf3b4168d
CynthiaStephan <cynthia.stephan@institutsolacroup.com>
2024-11-15 10:48:07 +0100

    init

---
+++ php/index.php
@@ -0,0 +1,12 @@
+<?php
+$host = 'my_name_mysql';
+$dbname = 'local';
+$username = 'devuser';
+$password = 'devpass';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion à la base de données : " . $e->getMessage());
+}
